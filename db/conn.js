29bb1a78bdc276ef8a79e145c0065d90fad95e1a
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-10-18 22:24:54 -0300

    update

--- db/conn.js
+++ db/conn.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize("freedb_SmartBin", "freedb_user-mobile" , "x3P$Q*pU*#EMaKj", { 
     port: 3306, 
     host: "sql.freedb.tech",
-    dialect: "sql.freedb.tech",
+    dialect: "mysql",
 });
 
 
